7108a6aacd81c12d9e2edf5d69e1c90b6dce66b5
Kasu <kasu@kasu-it.dev>
2024-01-04 17:00:38 -0600

    added tagline

--- client/src/modules/Home.tsx
+++ client/src/modules/Home.tsx
@@ -5,8 +5,12 @@ import '../styles/Home.css'
 function Home() {
   return <div className="Home">
     <div className='FrontPage'>
-      <div className='Tagline'></div>
+      <div className='Tagline'>
+        <h1 className='TaglineHeader'>Hershey Family Reunion 2024</h1>
+        <text>August 2nd - 4th</text>
+      </div>
     </div>
+    <div className='Info'></div>
   </div>;
 }
 

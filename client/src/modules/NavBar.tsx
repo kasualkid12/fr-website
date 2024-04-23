9161a5346f3554a389c8a343da1d9db099c0ff75
kasualkid12 <pdwalker3@gmail.com>
2024-04-23 18:45:53 -0500

    adjusted navbar to mobile first development

--- client/src/modules/NavBar.tsx
+++ client/src/modules/NavBar.tsx
@@ -1,20 +1,31 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import '../styles/NavBar.scss';
 import crest from '../public/Hershey Crest.512x(Transparent).png';
-import defaultPfp from '../public/Default user.svg'
+import defaultPfp from '../public/Default user.svg';
 
 function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="Navbar">
-      <div className="FamilySeal">
-        <img className="Crest" alt="The family crest" src={crest} />
+      <div className={`Hamburger ${isOpen ? 'open' : ''}`} onClick={() => setIsOpen(!isOpen)}>
+        <div className="Bar"></div>
+        <div className="Bar"></div>
+        <div className="Bar"></div>
       </div>
-      <div className='Title'>Hershey Reunion</div>
-      <a href="/">Home</a>
-      <a href="/family-tree">Family Tree</a>
+      <div className="CenterContent">
+        <div className="FamilySeal">
+          <img className="Crest" alt="The family crest" src={crest} />
+        </div>
+        <div className="Title">Hershey Reunion</div>
+      </div>
+      <div className={`DropdownMenu ${isOpen ? 'open' : ''}`}>
+          <a href="/">Home</a>
+          <a href="/family-tree">Family Tree</a>
+        </div>
+      {/* Can be added back when accounts are added */}
       <div className="ProfileImage">
-        <img className='Pfp' alt='pfp' src={defaultPfp} />
+        <img className="Pfp" alt="pfp" src={defaultPfp} />
       </div>
     </div>
   );

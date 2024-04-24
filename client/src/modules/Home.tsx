6bf8f8a65ab5247baffe8aafdb90c6edcdf21d47
kasualkid12 <pdwalker3@gmail.com>
2024-04-23 23:59:59 -0500

    fixed Home for all screen sizes and modularized

--- client/src/modules/Home.tsx
+++ client/src/modules/Home.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
-
-import '../styles/Home.scss'
+import LandingPage from './LandingPage';
 
 function Home() {
   return <div className="Home">
-    <div className='FrontPage'>
-      <div className='Tagline'>
-        <h1 className='TaglineHeader'>Hershey Family Reunion 2024</h1>
-        <text>August 2nd - 4th</text>
-      </div>
-    </div>
-    <div className='Info'></div>
+    <LandingPage />
   </div>;
 }
 
---
+++ client/src/modules/LandingPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import '../styles/LandingPage.scss'
+
+function LandingPage() {
+  return <div className="LandingPage">
+    <div className='FrontPage'>
+      <div className='Tagline'>
+        <h1 className='TaglineHeader'>Hershey Family Reunion 2024</h1>
+        <text>August 2nd - 4th</text>
+      </div>
+    </div>
+    <div className='Info'></div>
+  </div>;
+}
+
+export default LandingPage;
